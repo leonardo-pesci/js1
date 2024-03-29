407a5d0cda1f89d47884b2ac1b85f337fed17056
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-03-29 20:42:32 +0100

    buttons

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('hello world!')
+
+function left(){
+    let block = document.querySelector("#block");
+
+    block.style.left = 0;
+}
+
+function right(){
+    let block = document.querySelector("#block");
+
+    block.style.left = "350px";
+}
